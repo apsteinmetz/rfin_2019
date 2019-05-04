e17693d0a59e4b0e24cd7cbf42aa35b3d85fa726
apsteinmetz <apsteinmetz@yahoo.com>
2019-05-04 12:58:51 -0400

    use fresh data

--- r_finance_demo.R
+++ r_finance_demo.R
@@ -1,8 +1,68 @@
+#Using tidy_bdh()
+library(tidyverse)
+library(Rblpapi)
+
+secs <- c("SPX Index" , "LBUSTRUU Index")
+start_date <- as.Date("2008-09-01")
+end_date <- as.Date("2009-04-01")
+fields = "PX_LAST"
+bdh_opts = c("periodicitySelection"="MONTHLY")
+
+#must have a valid blp session running
+blpConnect() 
+bbg_dat <- bdh(secs,
+               start.date = start_date,
+               end.date = end_date, 
+               fields = fields,
+               options = bdh_opts)
+bbg_dat
+
+
+# -------------------------------------------------
+
+# Get Tidy BBG Data
+#Turn a list of data frames into a single "tidy" data frame.
+# CAUTION: The order of the returned secs series may not be the same as order of the secs inputs
+# CAUTION: There is no guarantee that the all securities will have values for the same dates.
+#          This is low risk at frequencies of weekly or lower
+tidy_bdh<-function(secs,...){
+  blpConnect() #must have a valid blp session running
+  blp_bdh  <-bdh(secs,...=...)
+  blp_bdh_tidy<-bind_rows(blp_bdh,.id='ticker') %>%
+    mutate(sector=word(ticker,-1)) %>% 
+    mutate(ticker=word(ticker,1)) %>% 
+    select(date,ticker,everything())%>%
+    group_by(sector,ticker)
+  #special case of one ticker
+  if (length(secs)==1){
+    blp_bdh_tidy$ticker=word(secs,1)
+    blp_bdh_tidy$sector=word(secs,-1)
+  }
+  
+  return(blp_bdh_tidy)
+}
+
+#get bbg data in tidy format
+bbg_dat_tidy <- tidy_bdh(secs,
+               start.date = start_date,
+               end.date = end_date, 
+               fields = fields,
+               options = bdh_opts)
+bbg_dat_tidy
+
+
+
+save(bbg_dat,file="data/bbg_dat.rdata")
+save(bbg_dat_tidy,file="data/bbg_dat_tidy.rdata")
+
+# -------------------------------------------------
+
+
 # demo of chart_cum_returns
 library(tidyverse)
 library(PerformanceAnalytics)
 source("utilities.r")
-load("loan_dur_Data_raw.RData")
+load("data/loan_dur_Data_raw.RData")
 index_names<-c("BarcAgg",
                "Tsy",
                "SPLoan",
@@ -53,4 +113,7 @@ r[106:110,]  %>% chart_cum_returns(shape="wide") +
   theme_minimal() +  theme(legend.position = "none")
 
 
-
+blp_bdh_tidy %>% 
+  group_by(ticker) %>% 
+  mutate(return=log(PX_LAST/lag(PX_LAST))) %>% 
+  ggplot(aes(date,return)) + geom_line()
